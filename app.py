bc5850320334ee247f649f8df3f7320eca0f93c3
CJ Fox <cjfox62600@gmail.com>
2021-10-03 22:00:59 -0400

    Wrote the flask methods to render html pages. Made sure flask is properly locating all images and stylesheets.

--- app.py
+++ app.py
@@ -3,8 +3,24 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def home():
     return render_template("index.html")
+
+@app.route("/index.html")
+def index():
+    return render_template("index.html")
+
+@app.route("/Bookmarks.html")
+def bookmarks():
+    return render_template("Bookmarks.html")
+
+@app.route("/About.html")
+def about():
+    return render_template("About.html")
+
+@app.route("/Contact.html")
+def contact():
+    return render_template("Contact.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
