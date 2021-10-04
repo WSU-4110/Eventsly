2127a24debd9de3b99e43dfa8d16cf9d326f3aa0
slattery-mark <markys1@hotmail.com>
2021-10-04 13:31:30 -0400

    working map on main page

---
+++ static/scripts/mapScripts.js
@@ -0,0 +1,16 @@
+// default to WSU coordinates
+var defaultLat = 42.3591
+var defaultLong = -83.0665
+
+var mymap = L.map('mapid').setView([defaultLat, defaultLong], 13);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  id: 'mapbox/streets-v11',
+  tileSize: 512,
+  zoomOffset: -1,
+  accessToken: 'pk.eyJ1IjoiY3NjNDExMGdyb3VwNSIsImEiOiJja3VjdmR4aWcxNGZjMzFvMzd2dHJhdnZmIn0.chI1y1ZmmmcdOX0nj1NtEQ'
+}).addTo(mymap);
+
+
