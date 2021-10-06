95549e133104d6fac449d4f2e280556380360753
CarbonatedSoftDrink <71098822+CarbonatedSoftDrink@users.noreply.github.com>
2021-10-05 20:09:08 -0400

    making imports from flask_sqlalchemy and setting up app.config for SQLAlchemy.

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'splite:///apple.db'
+db = SQLAlchemy(app)
 
 @app.route("/")
 def home():
