9ba0e068e7fba96e083554dc7a4dbc234df409d0
CarbonatedSoftDrink <71098822+CarbonatedSoftDrink@users.noreply.github.com>
2021-10-05 22:53:35 -0400

    created a class for holding account credentials.

--- app.py
+++ app.py
@@ -5,6 +5,14 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'splite:///apple.db' # the name is random. if you can think of a better one feel free to replace it.
 db = SQLAlchemy(app)
 
+class account(db.Model): # Creates a class to hold login information with.
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(30), nullable = False)
+    password = db.Column(db.String(30), nullable = False)
+
+    def __repr__(self): # Function that returns a string everytime a new element is created.
+        return '<Element %r>' % self.id
+
 @app.route("/")
 def home():
     return render_template("index.html")
