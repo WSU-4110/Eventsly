947c788417eac0d2021bda847cb3a3397d97b291
CJ Fox <cjfox62600@gmail.com>
2021-10-09 00:40:47 -0400

    Changed registration form to render using flask. Added 'includes' subfolder in 'templates' folder to include a form helper. Used the a form helper given from wtforms library's documentation.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, flash, url_for, session, logging
 from flask_mysqldb import MySQL
-from wtforms import Form, StringField, PasswordField, TelField, validators
+from wtforms import Form, StringField, PasswordField, validators
 from passlib.hash import sha256_crypt
 from datetime import datetime
 
@@ -9,7 +9,7 @@ app = Flask(__name__)
 class RegisterForm(Form):
     firstname = StringField('Firstname', [validators.Length(min = 1, max=50)])
     lastname = StringField('Lastname', [validators.Length(min = 1, max=50)])    
-    phone = TelField('Phone', [validators.Lenght(min=9,max=10)])
+    phone = StringField('Phone', [validators.Length(min=9,max=10)])
     username = StringField('Username', [validators.Length(min=4, max=30)])
     email = StringField('Email', [validators.Length(min=6, max=50)])
     password = PasswordField('Password', [
@@ -38,9 +38,14 @@ def about():
 def contact():
     return render_template("Contact.html")
 
-@app.route("/register.html", methods=['POST','GET'])
+@app.route("/register.html", methods=['GET','POST'])
 def register():
     form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        return render_template("register.html")
+    else:
+        return render_template("register.html", form=form)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
