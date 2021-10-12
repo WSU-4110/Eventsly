8281f6330e96987d93354aa7053951b1c63f47bf
slattery-mark <fe0846@wayne.edu>
2021-10-12 11:22:31 -0400

    applied responsive nav to each existing page

--- static/scripts/navScripts.js
+++ static/scripts/navScripts.js
@@ -1,4 +1,4 @@
-const hamburger = document.querySelector(".fa-bars");
-const dropdownMenu = document.querySelector(".nav-menu");
+var hamburger = document.querySelector(".fa-bars");
+var dropdownMenu = document.querySelector(".nav-menu");
 
 hamburger.addEventListener('click', function () { dropdownMenu.classList.toggle("dropdown") });
