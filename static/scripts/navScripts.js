90da6107b10d5954328ab75a3343ec1b276a5f76
slattery-mark <markys1@hotmail.com>
2021-10-12 14:09:48 -0400

    fixed event listener crashing before refresh

--- static/scripts/navScripts.js
+++ static/scripts/navScripts.js
@@ -1,4 +1,5 @@
-var hamburger = document.querySelector(".fa-bars");
-var dropdownMenu = document.querySelector(".nav-menu");
-
-hamburger.addEventListener('click', function () { dropdownMenu.classList.toggle("dropdown") });
+window.addEventListener('load', function () {
+    var hamburger = document.querySelector(".fa-bars");
+    var dropdownMenu = document.querySelector(".nav-menu");
+    hamburger.addEventListener('click', function () { dropdownMenu.classList.toggle("dropdown") });
+})
