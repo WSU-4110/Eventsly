bdc29d6fbe4fb1afbde6294f835921be10b05523
slattery-mark <fe0846@wayne.edu>
2021-10-12 11:13:07 -0400

    responsive nav animation

--- static/scripts/navScripts.js
+++ static/scripts/navScripts.js
@@ -1,8 +1,4 @@
 const hamburger = document.querySelector(".fa-bars");
 const dropdownMenu = document.querySelector(".nav-menu");
 
-hamburger.addEventListener('click', mobileMenu);
-
-function mobileMenu() {
-    dropdownMenu.classList.toggle("dropdown");
-}
+hamburger.addEventListener('click', function () { dropdownMenu.classList.toggle("dropdown") });
