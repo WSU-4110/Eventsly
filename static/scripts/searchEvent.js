db04b697575d352d6f52376f4b64fa6b1b596e02
CarbonatedSoftDrink <71098822+CarbonatedSoftDrink@users.noreply.github.com>
2021-10-26 16:45:11 -0400

    edits to js file.

--- static/scripts/searchEvent.js
+++ static/scripts/searchEvent.js
@@ -6,7 +6,7 @@ async function listEvents(req, res) {
     try {
         const db = await conn.connect();
         const result = await db.query('SELECT title FROM events order by title LIMIT 10');
-        const results = { users: (result) ? result.rows : null};
+        const results = { events: (result) ? result.rows : null};
         res.render('pages/search', results );
         db.release();
     } catch (err) {
