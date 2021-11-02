660ff97d8b9de5205db75907e238df0da40849d4
slattery-mark <fe0846@wayne.edu>
2021-11-02 15:06:51 -0400

    implementing basic frontend form validation

--- static/scripts/formScripts.js
+++ static/scripts/formScripts.js
@@ -1,25 +1,64 @@
-var inputs = document.getElementsByTagName('input');
-var labels = document.getElementsByTagName('label');
-var formItems = document.getElementsByClassName('form-item');
-
 window.onload = function () {
-    for (var i = 0; i < formItems.length; i++) {
-        (function () {
-            var label = formItems[i].getElementsByTagName('LABEL')[0];
-            var input = formItems[i].getElementsByClassName('form-input')[0];
-            var span = formItems[i].getElementsByClassName('counter')[0];
+    var formItems = document.getElementsByClassName('form-item');
+    var textInputs = [];
+    var submit = null;
+
+    // set up form label positions and counters
+    for (let i = 0; i < formItems.length; i++) {
+        {
+            let label = formItems[i].getElementsByTagName('LABEL')[0];
+            let input = formItems[i].getElementsByTagName('INPUT')[0];
+            let span = formItems[i].getElementsByClassName('counter')[0];
             if (label && input) {
-                console.log(label + ' ' + input.id + ' ' + span);
-                initLabelPos(input, label)
+                initLabelPos(input, label);
                 countChars(input, span); // initialize counters to 0
                 bindInputEvents(input, span, label);
+                textInputs.push(input);
             }
-        }());
+            else if (input.getAttribute('type') == 'submit') {
+                submit = input;
+            }
+        };
     }
+
+    // front-end form validation
+    var signup = document.getElementById('signup');
+    submit.classList.add('disable-submit');
+    submit.disabled = true;
+    if (signup) {
+        var formValidator = new InputValidatorService();
+        formValidator.addValidatorArray([
+            new FirstNameValidator(document.getElementById('firstname')),
+            new LastNameValidator(document.getElementById('lastname'))
+        ]);
+    }
+    signup.addEventListener('keyup', function () {
+        let results = formValidator.testValidity();
+        let validInputs = results[0];
+        let invalidInputs = results[1];
+        if (validInputs.length > 0) {
+            for (let i = 0; i < validInputs.length; i++) {
+                // do something - outline red?
+            }
+        }
+        if (invalidInputs.length > 0) {
+            for (let i = 0; i < invalidInputs.length; i++) {
+                // do something - outline green?
+            }
+            if (!submit.classList.contains('disable-submit')) {
+                submit.classList.add('disable-submit');
+                submit.disabled = true;
+            }
+        }
+        else {
+            submit.classList.remove('disable-submit');
+            submit.disabled = false;
+        }
+
+    });
 }
 
 function bindInputEvents(input, span, label) {
-    input.addEventListener('focusout', function () { isValid(input); });
     input.addEventListener('focusin', function () { moveLabelPos(label); });
     input.addEventListener('focusout', function () { defaultLabelPos(input, label); });
     input.addEventListener('input', function () { countChars(input, span); });
@@ -30,22 +69,13 @@ function countChars(input, span) {
     span.innerHTML = input.value.length + '/' + input.getAttribute('maxlength');
 }
 
-/* check if the length in an input text field is between its constraints */
-function isValid(input) {
-    if (input.value.length >= input.getAttribute('minlength') && input.value.length <= input.getAttribute('maxlength')) {
-        input.classList.remove('invalid');
-    }
-    else {
-        input.classList.add('invalid');
-    }
-}
-
-/* move the labels outside of the inputs on focus */
+/* move the labels outside of the inputs on focus or when text in input */
 function initLabelPos(input, label) {
     if (input.value.length > 0) {
         label.classList.add('move-label');
     }
 }
+
 function moveLabelPos(label) {
     if (!label.classList.contains('move-label')) {
         label.classList.add('move-label');
