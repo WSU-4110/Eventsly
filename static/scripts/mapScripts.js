6d296da65e8a9a967ef2a4bf727f2ce0129c9767
slattery-mark <fe0846@wayne.edu>
2021-11-10 08:42:11 -0500

    map decorator pattern fully implemented on index

--- static/scripts/mapScripts.js
+++ static/scripts/mapScripts.js
@@ -1,5 +1,14 @@
+/*----zoom levels----*/
+/* 0:	 The Earth     */
+/* 3:	 A continent   */
+/* 4:	 Large islands */
+/* 6:	 Large rivers  */
+/* 10: Large roads   */
+/* 15: Buildings     */
+/*-------------------*/
+
+
 // Decorater Design Pattern
-// NOTE: Typically, there would be an interface which BaseMap and Inter
 class BaseMap {
   constructor(mapid, initialLat, initialLng, initialZoom) {
     // initialize map, retrieve tiles, perform basic setup
@@ -22,21 +31,23 @@ class BaseMapDecorator {
   // constructor should take a parameter of type BaseMap 
   // but there is no way to enforce interfaces in JS
   constructor(baseMap) {
-    console.log(baseMap);
     if (baseMap instanceof BaseMap || baseMap instanceof BaseMapDecorator) {
       this.map = baseMap.map;
     }
-    else console.log(false)
+    else return false;
   }
 }
 
 class Geocoding extends BaseMapDecorator {
+  // adds geocoding functionality to the map
+  // including searching for locations
+  // and finding user location
   constructor(baseMap) {
     super(baseMap);
-    // search map feature
+    // search for address
     const geocoder = L.control.geocoder('pk.7963fb77afa804ed20fabb795cc1295d').addTo(this.map);
 
-    // find user location option
+    // find user location
     this.map.addControl(L.control.locate({
       locateOptions: {
         enableHighAccuracy: true
@@ -52,6 +63,8 @@ class Geocoding extends BaseMapDecorator {
 }
 
 class PinLoading extends BaseMapDecorator {
+  // adds the ability to load pins from the database
+  // parsed from JSON in HTML and sent to this function
   constructor(baseMap, pins) {
     super(baseMap);
     this.loadPins(pins);
@@ -60,7 +73,7 @@ class PinLoading extends BaseMapDecorator {
   loadPins(pins) {
     var marker
     for (let pin of pins) {
-      marker = new L.Marker([pin.latitude, pin.longitude]).addTo(map);
+      marker = new L.Marker([pin.latitude, pin.longitude]).addTo(this.map);
       marker.bindPopup(`<strong> ${pin.title}</strong><br>
       ${pin.city},${pin.state} <br>
       ${pin.date}<br>
@@ -70,6 +83,7 @@ class PinLoading extends BaseMapDecorator {
 }
 
 class PinPlacing extends BaseMapDecorator {
+  // adds the ability for the user to place pins on the map
   static marker;
   constructor(baseMap) {
     super(baseMap);
@@ -95,86 +109,3 @@ class PinPlacing extends BaseMapDecorator {
     }
   }
 }
-
-
-
-// defaultLat = 42.3591;
-//   defaultLng = -83.0665;
-// defaultZoom = 15;
-
-/*----zoom levels----*/
-/* 0:	 The Earth     */
-/* 3:	 A continent   */
-/* 4:	 Large islands */
-/* 6:	 Large rivers  */
-/* 10: Large roads   */
-/* 15: Buildings     */
-/*-------------------*/
-
-// all controls related to the leaflet map & user location fider
-// var map = L.map('mapid').setView([defaultLat, defaultLng], 15);
-
-
-
-// function buildMap() {
-//   // get map from mapbox
-//   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//     maxZoom: 18,
-//     id: 'mapbox/streets-v11',
-//     tileSize: 512,
-//     zoomOffset: -1,
-//     accessToken: 'pk.eyJ1IjoiY3NjNDExMGdyb3VwNSIsImEiOiJja3VjdmR4aWcxNGZjMzFvMzd2dHJhdnZmIn0.chI1y1ZmmmcdOX0nj1NtEQ'
-//   }).addTo(map);
-
-//   // remove double click zoom
-//   map.doubleClickZoom.disable();
-
-//   // find user location option
-//   map.addControl(L.control.locate({
-//     locateOptions: {
-//       enableHighAccuracy: true
-//     }
-//   }));
-
-
-// }
-
-// function loadPins(pins) {
-//   var marker
-//   for (let pin of pins) {
-//     marker = new L.Marker([pin.latitude, pin.longitude]).addTo(map);
-//     marker.bindPopup(`<strong> ${pin.title}</strong><br>
-//     ${pin.city},${pin.state} <br>
-//     ${pin.date}<br>
-//     <a href=eventdetails.html>View Details</a>`)
-//   }
-
-// }
-// function userTools() {
-//   // placeholder for user placed pin
-//   var marker
-
-//   // on click, either remove existing pin from search or placement
-//   // or place a new pin if there are no existing pins
-//   map.on('click', function (e) {
-//     if (geocoder.markers.length == 1) {
-//       geocoder.reset();
-//       geocoder.collapse();
-//     }
-//     else if (marker) {
-//       map.removeLayer(marker);
-//       marker = false;
-//     }
-//     else {
-//       marker = new L.Marker(e.latlng, { draggable: true }).addTo(map);
-
-//       // popup text
-//       marker.bindPopup("test");
-//     }
-//   });
-
-// }
-
-// buildMap();
-// userTools();
