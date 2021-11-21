9306066808aec9a866c2f2bf5e334791dfbf449a
CarbonatedSoftDrink <71098822+CarbonatedSoftDrink@users.noreply.github.com>
2021-11-20 23:18:29 -0500

    wrote tests checking that each page opens correctly.

--- testing/test_app.py
+++ testing/test_app.py
@@ -15,3 +15,38 @@ import models
 def test_SignUpIsFound(app,client):
     res=client.get('/signup.html')
     assert res.status_code == 200 #assert OK HTTP status code
+
+# tests for home() /////
+def test_home(client):
+    homepage = client.get('/')
+    assert homepage.status_code == 302
+
+# /////////////////////
+
+# tests for index() /////
+def test_index(client):
+    homepage = client.get("/index.html")
+    assert homepage.status_code == 200
+
+# /////////////////////
+
+# tests for aboutus() /////
+def test_aboutus(client):
+    page = client.get("/aboutus.html")
+    assert client.get("/aboutus/").data == page.data
+
+# /////////////////////
+
+# tests for search() /////
+def test_search(client):
+    page = client.get("/search.html")
+    assert page.status_code == 200
+
+# /////////////////////
+
+# tests for createEvent() /////
+def test_createEvent(client):
+    page = client.get("/createEvent.html")
+    assert page.status_code == 302
+
+# /////////////////////
